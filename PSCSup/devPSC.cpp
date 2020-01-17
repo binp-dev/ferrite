31d2d3cc9d87ad739e74c76171e7205906e3421c
Alexey Gerasev <a.v.gerasyov@inp.nsk.su>
2020-01-17 10:21:37 +0700

    handle exceptions

--- PSCSup/devPSC.cpp
+++ PSCSup/devPSC.cpp
@@ -1,58 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdlib>
+#include <iostream>
+#include <atomic>
+
 #include <devSup.h>
 #include <recGbl.h>
 #include <alarm.h>
-
-#include <waveformRecord.h>
-
+#include <epicsExit.h>
 #include <epicsExport.h>
-
 #include <iocsh.h>
+#include <waveformRecord.h>
 
 #include "framework.hpp"
 
-
-extern "C" {
-    void init(void);
-    long record_waveform_init(waveformRecord *record);
-    long record_waveform_get_ioint_info(int cmd, waveformRecord *record, IOSCANPVT *ppvt);
-    long record_waveform_read(waveformRecord *record);
-}
-
-/*
-void print_data(waveformRecord *record) {
-    printf("nelm: %d\n", record->nelm);
-    printf("nord: %d\n", record->nord);
-    printf("bptr: [");
-    int i = 0;
-    for (i = 0; i < record->nord; ++i) {
-        printf(" %.2lf, ", ((double*)record->bptr)[i]);
-    }
-    printf(" ]\n");
-}
-*/
 
 void init(void) {
     printf("init\n");
 }
 
 long record_waveform_init(waveformRecord *record) {
-    printf("record_waveform_init: %s\n", record->name);
-    std::unique_ptr<GenericWaveformRecord> user_record = framework_init_record<GenericWaveformRecord>(record->name);
+    std::cout << "record_waveform_init: " << record->name << std::endl;
+    std::unique_ptr<GenericWaveformRecord> user_record;
+
+    try {
+        user_record = framework_init_record<GenericWaveformRecord>(record->name);
+    } catch (const std::exception &e) {
+        std::cerr << "Exception caught" << std::endl;
+        std::cerr << e.what() << std::endl;
+        epicsExit(1);
+    } catch (...) {
+        std::cerr << "Unknown exception caught" << std::endl;
+        epicsExit(1);
+    }
+
     assert(record->ftvl == user_record->generic_type());
     record->dpvt = reinterpret_cast<void*>(user_record.release());
     return 0;
 }
 long record_waveform_get_ioint_info(int cmd, waveformRecord *record, IOSCANPVT *ppvt) {
-    printf("record_waveform_get_ioint_info: %s\n", record->name);
-    printf("unimplemented\n");
+    std::cout << "record_waveform_get_ioint_info: " << record->name << std::endl;
+    std::cerr << "unimplemented" << std::endl;
     return 0;
 }
 long record_waveform_read(waveformRecord *record) {
-    printf("record_waveform_read: %s\n", record->name);
+    std::cout << "record_waveform_read: " << record->name << std::endl;
     GenericWaveformRecord *user_record = reinterpret_cast<GenericWaveformRecord*>(record->dpvt);
-    user_record->read_generic(record->bptr, record->nord, record->ftvl);
+
+    try {
+        user_record->read_generic(record->bptr, record->nord, record->ftvl);
+    } catch (const std::exception &e) {
+        std::cerr << "Exception caught" << std::endl;
+        std::cerr << e.what() << std::endl;
+        epicsExit(1);
+    } catch (...) {
+        std::cerr << "Unknown exception caught" << std::endl;
+        epicsExit(1);
+    }
+
     return 0;
 }
 
