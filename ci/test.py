92cb805625a9e76c2f06688bee373eae9e18e977
Alexey Gerasev <a.v.gerasyov@inp.nsk.su>
2021-02-16 17:16:00 +0700

    Test ZMQ

--- ci/test.py
+++ ci/test.py
@@ -1,6 +1,38 @@
-#!/usr/bin/env python3
+import multiprocessing
+import zmq
 
-import time
 
-time.sleep(1)
-print("Success")
+addr = "tcp://127.0.0.1:5678"
+
+
+def ping():
+    """Sends ping requests and waits for replies."""
+    context = zmq.Context()
+    sock = context.socket(zmq.REQ)
+    sock.bind(addr)
+
+    for i in range(5):
+        sock.send_unicode("ping %s" % i)
+        rep = sock.recv_unicode()  # This blocks until we get something
+        print("Ping got reply:", rep)
+
+
+def pong():
+    """Waits for ping requests and replies with a pong."""
+    context = zmq.Context()
+    sock = context.socket(zmq.REP)
+    sock.connect(addr)
+
+    for i in range(5):
+        req = sock.recv_unicode()  # This also blocks
+        print("Pong got request:", req)
+        sock.send_unicode("pong %s" % i)
+
+
+if __name__ == "__main__":
+    pong_proc = multiprocessing.Process(target=pong)
+    pong_proc.start()
+
+    ping()
+
+    pong_proc.join()
