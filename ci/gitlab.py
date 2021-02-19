0d6b697bb6d5dbd52d7b5307edb873f5d38b3cf7
Alexey Gerasev <a.v.gerasyov@inp.nsk.su>
2021-02-19 20:18:04 +0700

    Run task without dependencies

--- ci/gitlab.py
+++ ci/gitlab.py
@@ -39,7 +39,7 @@ class Job(object):
             f"{self.name()}:",
             f"  stage: {self.stage()}",
             f"  script:",
-            f"    - python3 -u -m manage {self.name()}",
+            f"    - python3 -u -m manage --no-deps {self.name()}",
         )
 
         if len(self.deps) > 0:
