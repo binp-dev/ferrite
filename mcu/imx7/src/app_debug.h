095103ad7ecfb78545d732d13caf81ad6ec52c23
Alexey Gerasev <a.v.gerasyov@inp.nsk.su>
2021-07-06 20:54:01 +0700

    Fix iMX7 build with IPP

--- mcu/imx7/src/app_debug.h
+++ mcu/imx7/src/app_debug.h
@@ -1,12 +1,10 @@
 #pragma once
 
-
 #ifdef APP_DEBUG_IO_UART
 #include "debug_console_imx.h"
 #endif // APP_DEBUG_IO_UART
 
 #ifdef APP_DEBUG_IO_RPMSG
-#include <proto.h>
 #include <stdio.h>
 #include <stdint.h>
 #include "app_rpmsg.h"
@@ -16,6 +14,6 @@ void APP_Debug_IO_RPMSG_Send(int len);
 
 extern uint8_t __app_io_rpmsg_buffer[0x100];
 #define PRINTF(...) \
-    APP_Debug_IO_RPMSG_Send(snprintf((char*)__app_io_rpmsg_buffer + 2, 0x100 - 3, __VA_ARGS__))
+    APP_Debug_IO_RPMSG_Send(snprintf((char*)__app_io_rpmsg_buffer + 1, 0x100 - 2, __VA_ARGS__))
 
 #endif // APP_DEBUG_IO_RPMSG
