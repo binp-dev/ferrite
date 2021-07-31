bb04b4e23b69183b7b8cf5831a255f609b155951
Alexey Gerasev <a.v.gerasyov@inp.nsk.su>
2021-07-31 15:58:35 +0700

    Add some comments

--- app/src/record/base.hpp
+++ app/src/record/base.hpp
@@ -15,12 +15,15 @@ public:
 class Handler {
 public:
     virtual ~Handler() = default;
+    // Specifies if the record must be processed asynchronously.
     [[nodiscard]] virtual bool is_async() const = 0;
 };
 
+// Helper class for typed handler setting.
 template <typename H>
 class HandledRecord : public virtual Record {
 public:
     static_assert(std::is_base_of_v<Handler, H>);
+    // Sets a handler for the record.
     virtual void set_handler(std::unique_ptr<H> &&handler) = 0;
 };
--- ioc/PSCSup/base.hpp
+++ ioc/PSCSup/base.hpp
@@ -31,9 +31,13 @@ public:
 class EpicsRecord : public virtual Record
 {
 private:
+    // Raw record pointer. *Must be always valid.*
     dbCommon *const raw_;
+    // Callback for asynchronous processing.
     epicsCallback async_callback_;
+    // User-defined handler for the record. *Can be empty.*
     std::unique_ptr<Handler> handler_;
+    // Scan list for I/O Intr. *Empty if record scanning is not `I/O Intr`.*
     std::optional<ScanList> scan_list_;
 
 public:
@@ -44,19 +48,25 @@ public:
     EpicsRecord(const EpicsRecord &) = delete;
     EpicsRecord &operator=(const EpicsRecord &) = delete;
 
+    // Private data of the raw record stores `this`.
     static void set_private_data(dbCommon *raw, std::unique_ptr<EpicsRecord> &&record);
     static const EpicsRecord *get_private_data(const dbCommon *raw);
     static EpicsRecord *get_private_data(dbCommon *raw);
 
 protected:
+    // Process record synchronously (usually calls handler).
     virtual void process_sync() = 0;
+    // Pass I/O Intr callback to handler.
     virtual void register_processing_request();
 
 private:
+    // PACT flag for asynchronous record processing.
     bool is_processing_active() const;
     void set_processing_active(bool pact);
+    // Implicit record locking.
     [[nodiscard]] ScanLockGuard scan_lock();
 
+    // Internal methods for asynchronous processing pipeline. 
     void notify_async_processing_complete();
     void schedule_async_processing();
     void process_async();
@@ -64,18 +74,23 @@ private:
     void init_async_processing_callback(epicsCallback *callback);
 
 public:
+    // Raw record pointer.
     const dbCommon *raw() const;
     dbCommon *raw();
 
+    // Record handler pointer (can be `nullptr`).
     const Handler *handler() const;
     Handler *handler();
 
+    // The entry point for record processing.
     void process();
 
+    // Scan list of ther record (for I/O Intr).
     const std::optional<ScanList> &scan_list() const;
     std::optional<ScanList> &scan_list();
     void set_scan_list(std::optional<ScanList> &&scan_list);
 
+    // Initiate request processing (call I/O Intr).
     void request_processing();
 
 public:
