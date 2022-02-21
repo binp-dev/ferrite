4cdc6a69d2f8cbfa90331243961df407dfca3ea6
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-02-21 13:01:59 +0700

    Fix format issues

--- source/mcu/hal/include/hal/assert.h
+++ source/mcu/hal/include/hal/assert.h
@@ -3,9 +3,10 @@
 #include "panic.h"
 #include "log.h"
 
-#define hal_assert(expr) do { \
-    if (!(expr)) { \
-        hal_log_error("Assertion failed: " #expr); \
-        hal_panic(); \
-    } \
-} while(0)
+#define hal_assert(expr) \
+    do { \
+        if (!(expr)) { \
+            hal_log_error("Assertion failed: %s", #expr); \
+            hal_panic(); \
+        } \
+    } while (0)
