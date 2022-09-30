7748962778f82d3e64f4cf3d764ff0af41a72b27
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-09-30 14:32:43 +0700

    Move DoubleVec ReadStream to Tornado

--- source/app/src/misc/double_vec.rs
+++ source/app/src/misc/double_vec.rs
@@ -1,8 +1,9 @@
 use super::AsyncFlag;
 use async_std::sync::{Arc, Mutex, MutexGuard};
 use std::{
-    mem::swap,
+    mem::{swap, ManuallyDrop},
     ops::{Deref, DerefMut},
+    ptr,
 };
 
 pub struct DoubleVec<T> {
@@ -36,7 +37,7 @@ pub struct Writer<T> {
 impl<T> Writer<T> {
     pub async fn write(&self) -> WriteGuard<'_, T> {
         WriteGuard {
-            inner: self.buffer.lock().await,
+            buffer: self.buffer.lock().await,
             ready: &self.ready,
         }
     }
@@ -56,6 +57,7 @@ impl<T> Reader<T> {
     pub async fn try_swap(&mut self) -> bool {
         let mut guard = self.write.buffer.lock().await;
         if self.write.ready.take() {
+            self.buffer.clear();
             swap(guard.deref_mut(), &mut self.buffer);
             true
         } else {
@@ -63,15 +65,7 @@ impl<T> Reader<T> {
         }
     }
 }
-impl<T: Clone> Reader<T> {
-    pub fn into_stream(self) -> ReadStream<T> {
-        ReadStream {
-            buffer: self,
-            pos: 0,
-            cyclic: false,
-        }
-    }
-}
+
 impl<T> Deref for Reader<T> {
     type Target = Vec<T>;
     fn deref(&self) -> &Vec<T> {
@@ -80,8 +74,15 @@ impl<T> Deref for Reader<T> {
 }
 
 pub struct WriteGuard<'a, T> {
-    inner: MutexGuard<'a, Vec<T>>,
+    buffer: MutexGuard<'a, Vec<T>>,
     ready: &'a AsyncFlag,
+}
+impl<'a, T> WriteGuard<'a, T> {
+    pub fn discard(mut self) {
+        self.buffer.clear();
+        let mut self_ = ManuallyDrop::new(self);
+        unsafe { ptr::drop_in_place(&mut self_.buffer as *mut MutexGuard<'a, _>) };
+    }
 }
 impl<'a, T> Drop for WriteGuard<'a, T> {
     fn drop(&mut self) {
@@ -91,35 +92,11 @@ impl<'a, T> Drop for WriteGuard<'a, T> {
 impl<'a, T> Deref for WriteGuard<'a, T> {
     type Target = Vec<T>;
     fn deref(&self) -> &Vec<T> {
-        self.inner.deref()
+        self.buffer.deref()
     }
 }
 impl<'a, T> DerefMut for WriteGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Vec<T> {
-        self.inner.deref_mut()
-    }
-}
-
-pub struct ReadStream<T: Clone> {
-    buffer: Reader<T>,
-    pos: usize,
-    cyclic: bool,
-}
-impl<T: Clone> ReadStream<T> {
-    pub fn buffer(&self) -> &Reader<T> {
-        &self.buffer
-    }
-    pub async fn next(&mut self) -> Option<T> {
-        loop {
-            if self.pos < self.buffer.len() {
-                let value = self.buffer[self.pos].clone();
-                self.pos += 1;
-                break Some(value);
-            } else if self.buffer.try_swap().await || self.cyclic {
-                self.pos = 0;
-            } else {
-                break None;
-            }
-        }
+        self.buffer.deref_mut()
     }
 }
