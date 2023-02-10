3becf0983e9c14b0b9fd9f290f82d7c5bad04332
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-02-10 12:42:09 +0700

    Move MCU HAL to Tornado

--- source/mcu/hal/include/hal/imx7/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-//! FIXME: Configure HAL from user project.
-
-#define HAL_MU_IRQ_PRIORITY 3
--- source/mcu/hal/include/hal/time.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-uint32_t hal_busy_wait_ns(uint64_t ns);
