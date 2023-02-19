049e9ce29cc70b914461e6a9224ca3750d4fb514
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-02-19 21:40:12 +0700

    New task hash and repr

--- ferrite/components/newbase.py
+++ ferrite/components/newbase.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import Callable, TypeVar, Type, Tuple, Any, Dict, overload
+from types import FunctionType, MethodType
 from dataclasses import dataclass
+from inspect import signature
 
 
 class Context:
@@ -20,17 +22,31 @@ class Component:
 T = TypeVar("T", bound=Component, contravariant=True)
 
 
-@dataclass
+@dataclass(eq=False, repr=False)
 class FunctionTask(Task):
     func: Callable[[Context], None]
+
+    def __post_init__(self) -> None:
+        self.__name__ = self.func.__name__
+        self.__qualname__ = self.func.__qualname__
 
     def __call__(self, ctx: Context) -> None:
         self.func(ctx)
 
+    def __hash__(self) -> int:
+        return hash(self.func)
 
-@dataclass
+    def __repr__(self) -> str:
+        return self.func.__repr__()
+
+
+@dataclass(eq=False, repr=False)
 class UnboundedTask:
     method: Callable[[T, Context], None]
+
+    def __post_init__(self) -> None:
+        self.__name__ = self.method.__name__
+        self.__qualname__ = self.method.__qualname__
 
     @overload
     def __get__(self, obj: Component, type: type | None = None) -> Task:
@@ -52,14 +68,35 @@ class UnboundedTask:
     def __call__(self, owner: T, ctx: Context) -> None:
         self.method(owner, ctx)
 
+    def __hash__(self) -> int:
+        return hash(self.method)
 
-@dataclass
+    def __repr__(self) -> str:
+        return self.method.__repr__()
+
+
+@dataclass(eq=False, repr=False)
 class BoundedTask(Task):
     owner: Component
     inner: UnboundedTask
 
+    @property
+    def method(self) -> Callable[[Context], None]:
+        method: Callable[[Context], None] = self.inner.method.__get__(self.owner)
+        return method
+
+    def __post_init__(self) -> None:
+        self.__name__ = self.method.__name__
+        self.__qualname__ = self.method.__qualname__
+
     def __call__(self, ctx: Context) -> None:
         self.inner(self.owner, ctx)
+
+    def __hash__(self) -> int:
+        return hash(self.method)
+
+    def __repr__(self) -> str:
+        return self.method.__repr__()
 
 
 class CompMeta(type):
@@ -70,16 +107,32 @@ class CompMeta(type):
         return this
 
 
+@overload
 def task(func: Callable[[Context], None]) -> Task:
-    return FunctionTask(func)
+    ...
 
 
-def comptask(method: Callable[[T, Context], None]) -> UnboundedTask:
-    return UnboundedTask(method)
+@overload
+def task(func: Callable[[T, Context], None]) -> UnboundedTask:
+    ...
+
+
+def task(func: Any) -> Any:
+    params = signature(func).parameters
+    print(params)
+    if len(params) == 1:
+        return FunctionTask(func)
+    elif len(params) == 2:
+        return UnboundedTask(func)
+    else:
+        raise TypeError(f"{func} has wrong signature")
 
 
 def component(class_: type) -> Type[Component]:
     return class_
+
+
+ctx = Context()
 
 
 @task
@@ -87,21 +140,35 @@ def empty(ctx: Context) -> None:
     pass
 
 
+print(f"{empty = }")
+print(f"{hash(empty) = }")
+empty(ctx)
+
+
 @dataclass
 class Test(Component):
     x: int
 
-    @comptask
+    @task
     def task(self, ctx: Context) -> None:
         print(f"test task: {self.x}")
 
 
-print(f"{Test.task}")
+print(f"{Test.task = }")
+print(f"{hash(Test.task) = }")
 
-ctx = Context()
 test = Test(123)
 
-print(f"{test.task}")
+print(f"{test.task = }")
+print(f"{hash(test.task) = }")
+print(f"{hash(test.task) = }")
+
+a = Test(321)
+b = Test(456)
+print(f"{hash(a.task) = }")
+print(f"{hash(b.task) = }")
+a = b
+print(f"{hash(a.task) = }")
 
 test.task(ctx)
 
@@ -111,3 +178,5 @@ def take(ctx: Context, fn: Callable[[Context], None]) -> None:
 
 
 take(ctx, test.task)
+
+print({k: v for k, v in {k: getattr(test, k) for k in dir(test)}.items() if isinstance(v, Task)})
